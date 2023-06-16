8415c57aac531b8817badc815a7ad6ddc33fd5e1
choigicheol <kofpo900916@gmail.com>
2023-06-17 02:24:58 +0900

    준비중 문구 추가

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,28 @@
+import Head from "next/head";
 import React from "react";
 
 function Search() {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>로또 번호 랜덤 생성</title>
+        <meta
+          name="description"
+          content={
+            "생성한 로또 번호가 과거 1등 당첨번호중에 있었는지 확인 해 볼 수 있습니다."
+          }
+        ></meta>
+      </Head>
+      <div className="flexCenter">준비 중</div>
+      <style jsx>{`
+        .flexCenter {
+          display: flex;
+          width: 100%;
+          justify-content: center;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default Search;
