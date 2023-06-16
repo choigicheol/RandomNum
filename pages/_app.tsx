3e1b15d0920aae2b5e5296627244e99a6503320c
choigicheol <kofpo900916@gmail.com>
2023-06-17 02:23:55 +0900

    footer 영역 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useCallback, useState } from "react";
 import { AppProps } from "next/app";
 import Top from "@/components/Top";
 import NumberContext from "../components/context/ResultContext";
+import Footer from "@/components/Footer";
 
 function App({ Component, pageProps }: AppProps) {
   const [numbers, setNumbers] = useState<number[][]>([]);
@@ -29,16 +30,19 @@ function App({ Component, pageProps }: AppProps) {
         >
           <Component {...pageProps} />
         </NumberContext.Provider>
+        <Footer />
       </div>
       <style jsx>{`
         .container {
           display: flex;
           width: 100%;
           justify-content: center;
+          /* background-color: #ffffff; */
         }
         .center {
           max-width: 1000px;
           width: 100%;
+          /* background-color: #000000; */
         }
       `}</style>
     </div>
