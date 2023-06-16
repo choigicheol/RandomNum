28eba9711be03b0b6e2631356f58a04cfe64ebdc
choigicheol <kofpo900916@gmail.com>
2023-06-17 02:27:02 +0900

    임시 footer

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="main">
+      {/* <a href="https://www.flaticon.com/kr/free-icons/1-" title="1 번 아이콘">1 번 아이콘  제작자: juicy_fish - Flaticon</a> */}
+      <style jsx>{`
+        .main {
+          height: 100px;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Footer;
