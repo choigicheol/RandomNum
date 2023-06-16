b57d69ed51779297bf7bb309f50e0126542686a1
choigicheol <kofpo900916@gmail.com>
2023-06-17 02:25:46 +0900

    navBar router 추가

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import { Menu } from "semantic-ui-react";
 
 function NavBar() {
   const [activeItem, setActiveItem] = useState("home");
+  const router = useRouter();
+  const handleNavigation = (uri: string) => {
+    router.push(uri);
+  };
   return (
     <Menu inverted>
-      <Menu.Item
-        name="메인"
-        active={activeItem === "home"}
-        // onClick={this.handleItemClick}
-      />
+      <Menu.Item name="메인" onClick={() => handleNavigation("/")} />
       <Menu.Item
         name="번호생성"
-        active={activeItem === "make"}
-        // onClick={this.handleItemClick}
+        onClick={() => handleNavigation("production")}
       />
       <Menu.Item
         name="과거당첨번호 검색"
-        active={activeItem === "search"}
-        // onClick={this.handleItemClick}
+        onClick={() => handleNavigation("search")}
       />
     </Menu>
   );
