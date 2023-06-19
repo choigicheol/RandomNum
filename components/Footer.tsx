bb56c6ba09808af6436804708eedec870f638645
choigicheol <kofpo900916@gmail.com>
2023-06-19 20:54:23 +0900

    footer 구현

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { Divider } from "semantic-ui-react";
 
 function Footer() {
   return (
     <div className="main">
-      {/* <a href="https://www.flaticon.com/kr/free-icons/1-" title="1 번 아이콘">1 번 아이콘  제작자: juicy_fish - Flaticon</a> */}
+      <div className="mail">
+        {`문의 : `}
+        <a href="mailto:gigichoi0916@gmail.com" title="문의하기 이메일 주소">
+          gigichoi0916@gmail.com
+        </a>
+      </div>
+      <div>
+        <a href="https://www.flaticon.com/kr/free-icons/1-" title="1번 아이콘">
+          icon 출처
+        </a>
+      </div>
       <style jsx>{`
         .main {
-          height: 100px;
+          display: flex;
+          height: 60px;
+          align-items: center;
+          padding: 10px;
+          margin-top: 14px;
+        }
+        .mail {
+          flex: 1;
+          text-align: center;
         }
       `}</style>
     </div>
