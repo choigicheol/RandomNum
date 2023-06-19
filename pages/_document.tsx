b19bf28cc098f71e75b6abbfab983b92d6bfa646
choigicheol <kofpo900916@gmail.com>
2023-06-19 18:51:38 +0900

    og tag 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,14 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="./favicon.ico" />
+          <meta property="og:title" content="로또 번호 랜덤 생성" />
+          <meta property="og:url" content="https://random-num.vercel.app/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="./ogImage.png" />
+          <meta
+            property="og:description"
+            content="제외하고 싶은 숫자를 빼고 로또 번호를 랜덤으로 생성할 수 있습니다."
+          />
         </Head>
         <body>
           <Main />
